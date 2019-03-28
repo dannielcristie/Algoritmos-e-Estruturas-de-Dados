90008885264c953a7e29fa13442f4f0b85ef52ef
Danniel <dany14012@gmail.com>
2019-03-28 01:16:45 +0000

    atvs

--- 01 - Revisão/04.py
+++ 01 - Revisão/04.py
@@ -1,4 +1,7 @@
+#coding=utf-8
 '''
 Escreva um programa que mostre todos os multiplos de 5 entre 0 e 50.
 
 '''
+for  i in range(0,51,5):
+    print(i)
--- 01 - Revisão/05.py
+++ 01 - Revisão/05.py
@@ -1,6 +1,10 @@
+#coding=utf-8
 '''
 Escreva um programa que mostre os dias da semana.
 
 '''
 
 dias_da_semana = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sábado', 'domingo']
+
+for e in dias_da_semana:
+    print(e)
