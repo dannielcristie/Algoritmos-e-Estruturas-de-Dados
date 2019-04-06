f0c081c8769dbf010ca2c65e515f6d94bab5b859
Danniel <dany14012@gmail.com>
2019-04-06 16:09:02 +0000

    upgrade0604

--- 02 - Listas/05.py
+++ 02 - Listas/05.py
@@ -1,4 +1,15 @@
+#coding=utf-8
 '''
-5) Dada uma lista L de números inteiros, escreva uma função que retorne uma lista P contendo os itens de L sem repetição
+5) 
+Dada uma lista L de números inteiros, escreva uma função que retorne uma lista P contendo os itens de L sem repetição
+'''
+def removerepetidos(lista):
+    l = []
+    for i in lista:
+        if i not in l:
+            l.append(i)
+    return l
 
-'''
+lista = [1, 1, 2, 1, 3, 4, 3, 6, 7, 6, 7, 8, 10 ,9]
+print(lista)
+print(removerepetidos(lista))
--- 02 - Listas/06.py
+++ 02 - Listas/06.py
@@ -1,5 +1,10 @@
+#coding=utf-8
 '''
 6) Dada uma lista L de números inteiros, escreva uma função que imprima os índices de todas as
 ocorrências de um número especificado.
 
 '''
+#def indiceespecifico(lista)
+lista = [6,1,2,3,5,6]
+for i in lista:
+    print(lista[i])
