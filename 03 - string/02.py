78c19e3bf0ef156aa4679c2b52b26bf62f466285
Danniel <dany14012@gmail.com>
2019-04-08 23:23:12 +0000

    ex02str

--- 03 - string/02.py
+++ 03 - string/02.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 '''
 2) Exercício - Separação de String
 Elabore um script que leia o nome completo de uma pessoa e imprima o prenome e o sobrenome
@@ -11,3 +12,17 @@ nome de um indivíduo, que antecede o nome de família; nome de batismo, antenom
 "Rui é prenome de Rui Barbosa"
 
 '''
+
+name = input('Digite seu nome:\t').lower()
+cont = 0
+l = []
+for i in name:
+    if i ==' ':
+        cont+=1
+        l.append(name[i])
+        i = cont
+        
+print(cont)
+print(name)
+print(type(i))
+print(l)
