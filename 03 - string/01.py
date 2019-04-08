0e6f9f08c7ed9ce0bf2979c7a525b80f1085c15b
Danniel <dany14012@gmail.com>
2019-04-08 18:16:21 +0000

    ex01str

--- 03 - string/01.py
+++ 03 - string/01.py
@@ -1,4 +1,15 @@
+#coding=utf-8
 '''
 1) Exercício - Contagem de Ocorrências
 A partir de um texto fornecido pelo usuário, conte o número de caracteres e quantos deles são vogais.
 '''
+
+text = (input('Digite um texto:\t')).lower()
+n = len(text)
+cont = 0
+vogais = ['a','e','i','o','u']
+for i in text:
+    if i in vogais: 
+        cont+=1
+print('A quantidade de caracteres é:\t',n)
+print('\nA quantidade de vogais é:\t',cont)
