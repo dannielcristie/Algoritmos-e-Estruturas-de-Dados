ecf37fb3f3ff2e275e0fb2a756381680b168b957
Danniel Cristie <dany14012@gmail.com>
2019-04-08 22:16:29 -0300

    ex03str

--- 03 - string/03.py
+++ 03 - string/03.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 '''
 3) Exercício - Inversão Maiuscula
 Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando somente letras maiúsculas.
@@ -7,3 +8,5 @@ Entrada de dados: Alana
 Saída de dados: ANALA
 
 '''
+name = str(input('Digite o seu nome:')).upper()
+print(name[::-1])
